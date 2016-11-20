a6df776d4d7406d402e1909f056df730dd4d84e4
Patryk Małek <malekpatryk@gmail.com>
2016-11-20 15:58:31 +0100

    Fixing travis build

--- scripts/install-boost.sh
+++ scripts/install-boost.sh
@@ -3,14 +3,14 @@
 # 1) BOOST_ROOT and BOOST_URL are already defined,
 # and contain valid values.
 # 2) The last namepart of BOOST_ROOT matches the
-# folder name internal to boost's .tar.gz
+# folder name internal to boost's .tar.bz2
 set -eu
 if [ ! -d "$BOOST_ROOT/lib" ]
 then
-  wget $BOOST_URL -O /tmp/boost.tar.gz
+  wget --no-check-certificate $BOOST_URL -O /tmp/boost.tar.bz2
   cd `dirname $BOOST_ROOT`
   rm -fr ${BOOST_ROOT}
-  tar xzf /tmp/boost.tar.gz
+  tar -xf /tmp/boost.tar.bz2
 
   params="define=_GLIBCXX_USE_CXX11_ABI=0 \
           --with-program_options --with-system --with-coroutine --with-filesystem"
